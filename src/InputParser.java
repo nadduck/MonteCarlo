ac8cb7e43acb59edaeef7832d3a10ab1a681aeba
John Shumway <john.shumwayjr@gmail.com>
2012-07-27 19:21:48 -0500

    Refactored input parser and tests.

--- src/InputParser.java
+++ src/InputParser.java
@@ -2,46 +2,85 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import java.io.File;
 import java.io.IOException;
 
 public class InputParser {
-	private SimulationInfo simulationInfo;
-	private File input;
 
-	public SimulationInfo readXML() throws ParserConfigurationException, SAXException, IOException {
-		input = new File("input.xml");
+	private Document doc;
+
+    public SimulationInfo parseXMLFromFile(String inputFileName) 
+	        throws ParserConfigurationException, SAXException, IOException {
+        Document doc = getXMLDocumentFromFile(inputFileName);
+		
+		SimulationInfo simulationInfo = parseXML(doc);
+        return simulationInfo;
+	}
+
+    public Document getXMLDocumentFromFile(String inputFileName) 
+            throws ParserConfigurationException, SAXException, IOException{
+        File input = new File(inputFileName);
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(input);
+        return doc;
+    }
+
+    public SimulationInfo parseXML(Document document) {
+        this.doc = document;
+        
+        double kT = getTemperature();
+		int sliceCount = getSliceCount();
+		double angfreq = getFrequency();
+		double mass = getMass();
 		
-		NodeList temp = doc.getElementsByTagName("Temperature");
-		String Temperature  = temp.item(0).getAttributes().getNamedItem("value").getNodeValue();
-		
-		NodeList slice = doc.getElementsByTagName("Path");
-		String numslice  = slice.item(0).getAttributes().getNamedItem("sliceCount").getNodeValue();
-		
-		NodeList osc = doc.getElementsByTagName("Oscillator");
-		String oscillator  = osc.item(0).getAttributes().getNamedItem("frequency").getNodeValue();
-		
-		NodeList masslist = doc.getElementsByTagName("Mass");
-		String massval  = masslist.item(0).getAttributes().getNamedItem("value").getNodeValue();
-		
-		double kT = Double.parseDouble(Temperature);
-		int sliceCount = Integer.parseInt(numslice);
-		double angfreq = Double.parseDouble(oscillator);
-		double mass = Double.parseDouble(massval);
-		
-		simulationInfo = new SimulationInfo();
+		SimulationInfo simulationInfo = new SimulationInfo();
 		simulationInfo.setkT(kT);
 		simulationInfo.setAngfreq(angfreq);
 		simulationInfo.setSliceCount(sliceCount);
 		simulationInfo.setMass(mass);
 		
 		return simulationInfo;
-	}
+    }
+
+    public double getTemperature() {
+        return getDoubleAttribute("Temperature", "value");
+    }
+
+    public int getSliceCount() {
+        return getIntegerAttribute("Path", "sliceCount");
+    }
+
+    public double getMass() {
+        return getDoubleAttribute("Mass", "value");
+    }
+
+    public double getFrequency() {
+        return getDoubleAttribute("Oscillator", "frequency");
+    }
+
+    public String getStringAttribute(String nodeName, String attributeName) {
+        NodeList osc = doc.getElementsByTagName(nodeName);
+    	Node node = osc.item(0);
+        NamedNodeMap attributes = node.getAttributes();
+        Node attribute = attributes.getNamedItem(attributeName);
+        String string = attribute.getNodeValue();
+        return string;
+    }
+
+    public int getIntegerAttribute(String nodeName, String attributeName) {
+        String string = getStringAttribute(nodeName, attributeName);
+        return Integer.parseInt(string);
+    }
+
+    public double getDoubleAttribute(String nodeName, String attributeName) {
+        String string = getStringAttribute(nodeName, attributeName);
+		return Double.parseDouble(string);
+    }
 
 
 }
--- test/InputParserTest.java
+++ test/InputParserTest.java
@@ -1,73 +1,88 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-import java.io.File;
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
+import org.junit.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 
 public class InputParserTest {
-       
-    private DocumentBuilderFactory docFactory;
-	private DocumentBuilder docBuilder;
-	private TransformerFactory transformerFactory;
-	private Transformer transformer;
-	private DOMSource source;
-	private StreamResult result;
-	private InputParser inputParser;
-	private SimulationInfo simulationInfo;
-	private SimulationInfo expectedSimInfo;
 
-	@Test
-    public void testReadXML() throws ParserConfigurationException, TransformerException, SAXException, IOException {
-        createXML();
+    private Document doc;
+    private Double temperature;
+    private Integer sliceCount;
+    private Double frequency;
+    private Double mass;
+    private InputParser inputParser;
+    private SimulationInfo expectedSimInfo;
+
+    @Before
+    public void before() throws ParserConfigurationException {
+        temperature = 0.25;
+        sliceCount = 20;
+        frequency = 0.8;
+        mass = 0.1;
+        createXMLDocument();
         inputParser = new InputParser();
-        expectedSimInfo = new SimulationInfo();
-        simulationInfo = inputParser.readXML();
-        assertEquals(expectedSimInfo.getAngfreq(), simulationInfo.getAngfreq(),1e-14);
-        assertEquals(expectedSimInfo.getkT(), simulationInfo.getkT(),1e-14);
-        assertEquals(expectedSimInfo.getSliceCount(), simulationInfo.getSliceCount(),1e-14);
-        assertEquals(expectedSimInfo.getMass(), simulationInfo.getMass(),1e-14);
+        expectedSimInfo = createSimulationInfo();
     }
 
-	private void createXML() throws ParserConfigurationException, TransformerException {
-		docFactory = DocumentBuilderFactory.newInstance();
-		docBuilder = docFactory.newDocumentBuilder();
-		
-		Document doc = docBuilder.newDocument();
-		Element rootElement = doc.createElement("MonteCarlo");
-		doc.appendChild(rootElement);
-		
-		addNodeAttribute(doc, rootElement, "Temperature", "value", "0.5");
-		addNodeAttribute(doc, rootElement, "Path", "sliceCount", "10");
-		addNodeAttribute(doc, rootElement, "Oscillator", "frequency", "1.0");
-		addNodeAttribute(doc, rootElement, "Mass", "value", "1.0");
+    @Test
+    public void testFrequency() {
+        SimulationInfo simulationInfo = inputParser.parseXML(doc);
+        assertEquals(expectedSimInfo.getAngfreq(), simulationInfo.getAngfreq(),
+                1e-14);
+    }
 
-		transformerFactory = TransformerFactory.newInstance();
-		transformer = transformerFactory.newTransformer();
-		source = new DOMSource(doc);
-		result = new StreamResult(new File("input.xml"));
-		
-		transformer.transform(source, result);
-	}
+    @Test
+    public void testTemperature() {
+        SimulationInfo simulationInfo = inputParser.parseXML(doc);
+        assertEquals(expectedSimInfo.getkT(), simulationInfo.getkT(), 1e-14);
+    }
 
-	private void addNodeAttribute(Document doc, Element rootElement, String elemTag, String attrTag, String attrVal) {
-		Element elem = doc.createElement(elemTag);
-		rootElement.appendChild(elem);
-		Attr elemAttr = doc.createAttribute(attrTag);
-		elemAttr.setValue(attrVal);
-		elem.setAttributeNode(elemAttr);
-	}
+    @Test
+    public void testSliceCount() {
+        SimulationInfo simulationInfo = inputParser.parseXML(doc);
+        assertEquals(expectedSimInfo.getSliceCount(),
+                simulationInfo.getSliceCount(), 1e-14);
+    }
+
+    @Test
+    public void testMass() {
+        SimulationInfo simulationInfo = inputParser.parseXML(doc);
+        assertEquals(expectedSimInfo.getMass(), simulationInfo.getMass(), 1e-14);
+    }
+
+    private SimulationInfo createSimulationInfo() {
+        SimulationInfo simulationInfo = new SimulationInfo();
+        simulationInfo.setkT(temperature);
+        simulationInfo.setSliceCount(sliceCount);
+        simulationInfo.setAngfreq(frequency);
+        simulationInfo.setMass(mass);
+        return simulationInfo;
+    }
+
+    private void createXMLDocument() throws ParserConfigurationException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory
+                .newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("MonteCarlo");
+        doc.appendChild(rootElement);
+
+        addNodeWithAttribute("Temperature", "value", temperature.toString());
+        addNodeWithAttribute("Path", "sliceCount", sliceCount.toString());
+        addNodeWithAttribute("Oscillator", "frequency", frequency.toString());
+        addNodeWithAttribute("Mass", "value", mass.toString());
+    }
+
+    private void addNodeWithAttribute(String elemTag, String attrTag,
+            String attrVal) {
+        Element elem = doc.createElement(elemTag);
+        Element rootElement = doc.getDocumentElement();
+        rootElement.appendChild(elem);
+        Attr elemAttr = doc.createAttribute(attrTag);
+        elemAttr.setValue(attrVal);
+        elem.setAttributeNode(elemAttr);
+    }
 
 }
