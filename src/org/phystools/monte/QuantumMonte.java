2f62699715f91b87b4bdc212762f68e96aa92a91
John Shumway <john.shumwayjr@gmail.com>
2012-08-14 14:53:24 -0700

    Small formatting change.

--- src/org/phystools/monte/QuantumMonte.java
+++ src/org/phystools/monte/QuantumMonte.java
@@ -1,4 +1,5 @@
 package org.phystools.monte;
+
 import java.io.IOException;
 import java.util.Random;
 
@@ -12,16 +13,14 @@ import org.phystools.monte.path.Path;
 import org.xml.sax.SAXException;
 
 public class QuantumMonte {
-    
+
     private Advancer advancer;
-	private Measurer measurer;    
-     
-	
+    private Measurer measurer;
+
     QuantumMonte(Advancer advancer, Measurer measurer) {
         this.advancer = advancer;
         this.measurer = measurer;
     }
-
 
     private void run(DataManager dataManager) throws IOException {
         dataManager.openOutputFile("something.dat");
@@ -32,43 +31,43 @@ public class QuantumMonte {
         dataManager.close();
     }
 
+    public static void main(String[] args) throws IOException,
+            ParserConfigurationException, SAXException {
+        InputParser inputParser = new InputParser();
+        SimulationInfo simulationInfo = inputParser
+                .parseXMLFromFile("input.xml");
 
-    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {     
-    	InputParser inputParser = new InputParser();
-    	SimulationInfo simulationInfo = inputParser.parseXMLFromFile("input.xml");
-    	
-		double kT = simulationInfo.getkT();
+        double kT = simulationInfo.getkT();
         int sliceCount = simulationInfo.getSliceCount();
-        
+
         int ndim = 2;
         GeometryFactory factory = createGeometryFactory(ndim);
         Path path = new Path(sliceCount, kT, factory);
-        
-        double deltaTau = path.getDeltaTau();        
+
+        double deltaTau = path.getDeltaTau();
         double mass = simulationInfo.getMass();
         double angfreq = simulationInfo.getAngfreq();
         Action action = simulationInfo.getAction();
 
-        Random rand = new Random(2012L);        
+        Random rand = new Random(2012L);
         double delta = 1.0;
         Mover mover = new UniformMover(rand, delta);
-        
-        Estimator potential = new PotentialEnergyEstimator(path, action, mass);
-		Estimator kinetic = new KineticEnergyEstimator(path, action, mass);
-		Estimator total = new TotalEnergyEstimator(potential, kinetic);
-		
-        Measurer measurer = new Measurer(path, total);
-        
-        mover = new GaussianMover(rand, deltaTau, mass, factory);
-        
-        Advancer advancer = new Advancer(path, action, mover, rand);
-    	
-		QuantumMonte qmc = new QuantumMonte(advancer, measurer);
-		
-		DataManager dataManager = new DataManager(kT, angfreq, mass, action);
-		qmc.run(dataManager);
-	}
 
+        Estimator potential = new PotentialEnergyEstimator(path, action, mass);
+        Estimator kinetic = new KineticEnergyEstimator(path, action, mass);
+        Estimator total = new TotalEnergyEstimator(potential, kinetic);
+
+        Measurer measurer = new Measurer(path, total);
+
+        mover = new GaussianMover(rand, deltaTau, mass, factory);
+
+        Advancer advancer = new Advancer(path, action, mover, rand);
+
+        QuantumMonte qmc = new QuantumMonte(advancer, measurer);
+
+        DataManager dataManager = new DataManager(kT, angfreq, mass, action);
+        qmc.run(dataManager);
+    }
 
     private static GeometryFactory createGeometryFactory(int n) {
         GeometryFactory factory = null;
